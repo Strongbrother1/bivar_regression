46cbbb5fc017c46d11978f69a669e9784de742df
Strongbrother1 <calvinmaycelee@gmail.com>
2022-11-29 20:35:56 -0600

    Equation works and prints out

--- project.py
+++ project.py
@@ -2,6 +2,7 @@
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn import linear_model
 # dataframes
 import pandas as pd
 # computation
@@ -16,6 +17,7 @@ df = pd.read_csv("project.csv")
 
 x_vals = df[['X', 'Y']].values
 y_vals = df['Z'].values
+
 
 print(x_vals[0], y_vals[0])
 
@@ -36,13 +38,24 @@ reg_mod = LinearRegression()
 reg_mod.fit(poly_x_vals, y_vals)
 
 y_pred = reg_mod.predict(poly_x_vals)
-reg_mod.coef_
+
+print('Intercept: \n', reg_mod.intercept_)
+print('Coefficients \n', reg_mod.coef_)
 
 mean_squared_error(y_vals, y_pred, squared=False)
 
+test_x = 87.54096453008634
+test_y = -77.77999732554369
+test_a = 9.26618031e-04
+test_b = -1.00113557e+00
+test_a2 = 1.20000059e+01
+test_ab = 2.99999416e+00
+test_b2 = -6.03159917e-07
+equation = reg_mod.intercept_ + (test_a * test_x) + (test_b * test_y) + (test_a2 * test_x * test_x) + (test_ab * test_x * test_y) + (test_b2 * test_y * test_y)
 
+print(equation)
 # check our accuracy for each degree, the lower the error the better!
-number_degrees = [1,2,3,4,5,6,7]
+"""number_degrees = [1,2,3,4,5,6,7]
 plt_mean_squared_error = []
 for degree in number_degrees:
 
@@ -55,12 +68,22 @@ for degree in number_degrees:
    regression_model.fit(poly_x_values, y_vals)
    y_pred = regression_model.predict(poly_x_values)
   
-   plt_mean_squared_error.append(mean_squared_error(y_vals, y_pred, squared=False))
+   plt_mean_squared_error.append(mean_squared_error(y_vals, y_pred, squared=False))"""
   
-plt.scatter(number_degrees,plt_mean_squared_error, color="green")
-plt.plot(number_degrees,plt_mean_squared_error, color="red") 
+# plt.scatter(number_degrees,plt_mean_squared_error, color="green")
+# plt.plot(number_degrees,plt_mean_squared_error, color="red") 
 
-"""fit = np.polyfit(df['X'], df['Y'], 2)
-equation = np.poly1d(fit)
-print ("The fit coefficients are a = {0:.4f}, b = {1:.4f} c = {2:.4f}".format(*fit))
-print (equation)"""
+
+"""
+1
+a = 9.26618031e-04
+b = -1.00113557e+00
+a^2 = 1.20000059e+01
+ab = 2.99999416e+00
+b^2 = -6.03159917e-07
+
+
+20.129297290460755,14.059520911264144,5707.43434233776
+
+1 + 7663 x 20 + 6050 x 14 + -6.8
+"""
